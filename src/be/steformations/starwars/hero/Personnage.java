7c4497b41b0f16678bb31da00ce8ac0e2003e53d
webco07 <webco07@prim2010-01.fapse.priv>
2018-02-19 12:11:03 +0100

    creation de la classe Personnage

---
+++ src/be/steformations/starwars/hero/Personnage.java
@@ -0,0 +1,24 @@
+package be.steformations.starwars.hero; // tout fichier java commence toujours par le package
+
+import java.util.Scanner; // package contenant scanner
+
+public abstract class Personnage{
+
+	private String nom;
+
+	public Personnage(String aNom){
+
+		this.nom = aNom; // copie de la valeur reçue en paramètre dans l'objet (variable d'instance nom)
+
+	}//fin constructeur
+	public String getnom(){
+
+		return this.nom;
+	
+	}//fin getnom
+	
+	public abstract void afficheCamp();
+
+	public abstract void combattre();
+	
+}
